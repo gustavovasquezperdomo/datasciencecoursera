c02055cb2283398da5ef5844fc5ddc858438bdcb
gustavovasquezperdomo <gustavo.vasquez@colombia.com>
2014-04-27 15:55:04 -0500

    adding comments to R assignment files
    Some comments added to the functions created before

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,11 +1,12 @@
+# This is a function to solve the inverse of a matrix
 cacheSolve <- function(x, ...) {
-  m <- x$getsolve()
+  m <- x$getsolve()   # If there is data in cache, then nothing is done
   if(!is.null(m)) {
     message("getting cached data")
-    return(m)
+    return(m) #Stop execution and return the cache value
   }
-  data <- x$get()
-  m <- solve(data, ...)
+  data <- x$get() 
+  m <- solve(data, ...) # If cacheSolve can't find a value in cache, then inverse 
   x$setsolve(m)
-  m
+  m  # Return m, as the inverse of x matrix
 }
--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,12 +1,14 @@
+# This is a function to create the cache of the inverse of a matrix
 makeCacheMatrix <- function(x = numeric()) {
   m <- NULL
   set <- function(y) {
     x <<- y
     m <<- NULL
-  }
+  } # I don't understand well this part of getters and setters
   get <- function() x
   setsolve <- function(solve) m <<- solve
   getsolve <- function() m
+  # Next part creates a list of get, set, getsolve an setsolve
   list(set = set, get = get,
        setsolve = setsolve,
        getsolve = getsolve)
