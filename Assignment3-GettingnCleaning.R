614f656f68c1bde31a21890c1dba83ec22c868cf
gustavovasquezperdomo <gustavo.vasquez@colombia.com>
2014-04-27 18:25:06 -0500

    ass 3 getting and cleaning
    changing directories to the new laptop

--- Assignment3-GettingnCleaning.R
+++ Assignment3-GettingnCleaning.R
@@ -1,43 +1,47 @@
 # This file is to solve the assignment of the week 3 in GettingnCleaningData
 
 
-setwd("D:/aToCoursera")
+setwd("C:/Users/UER/Documents/homework")
 
 
 list.files()
 i=1
 
-#=====================================================================
-setwd("D:/aToCoursera/UCI HAR Dataset/")
+# loading activity and feature names
+setwd("C:/Users/UER/Documents/homework/UCI_Dataset")
 activity_name <- read.table("activity_labels.txt")
 feature_name  <- read.table("features.txt")
 
-setwd("D:/aToCoursera/UCI HAR Dataset/test/")
+# Loading data_test and adding the column names to the data test
+setwd("C:/Users/UER/Documents/homework/UCI_Dataset/test/")
 data_test <- cbind(read.table("subject_test.txt"), read.table("y_test.txt"), read.table("X_test.txt"))
 colnames(data_test) <- c("subject", "activity", 1:561)
 
-setwd("D:/aToCoursera/UCI HAR Dataset/train/")
+# Binding subject and y train 
+setwd("C:/Users/UER/Documents/homework/UCI_Dataset/train/")
 data_train <- cbind(read.table("subject_train.txt"), read.table("y_train.txt"), read.table("X_train.txt"))
 colnames(data_train) <- c("subject", "activity", 1:561)
 
-setwd("D:/aToCoursera/UCI HAR Dataset/test/Inertial Signals")
+
+setwd("C:/Users/UER/Documents/homework/UCI_Dataset/test/Inertial Signals")
 file_list <- list.files()
 for (i in length(file_list)){
-  col_names <- c(colnames(data_test), rep(file_list[i], 128)) ## <<=================
+  col_names <- c(colnames(data_test), rep(file_list[i], 128))  
   data_test <- cbind(data_test, read.table(file_list[i]))
   colnames(data_test) <- col_names
 }
 
-setwd("D:/aToCoursera/UCI HAR Dataset/train/Inertial Signals")
+setwd("C:/Users/UER/Documents/homework/UCI_Dataset/Inertial Signals")
 file_list <- list.files()
 for (i in length(file_list)){
   col_names <- c(colnames(data_train), rep(file_list[i], 128))
   data_train <- cbind(data_train, read.table(file_list[i]))
-  colnames(data_train) <- col_names
+  colnames(data_train) <- col_names # adding colnames to data train
 }
 
-colnames(data_test) <- colnames(data_train)
+colnames(data_test) <- colnames(data_train) # Copying colnames from data train to data tes
 total_data <- rbind(data_test, data_train)
+
 
 library(reshape2)
 total_raw <- melt(total_data, id=1:563, na.rm=T)
